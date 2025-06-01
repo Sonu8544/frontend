0a71da2035dfa7b429db8603c33f9613409577a8
Sonu Kumar <sksonu8544@gmail.com>
2025-06-01 08:01:21 +0530

    dev signup form and feed intrested/ignore feature

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,8 +6,13 @@ import { useNavigate } from 'react-router-dom';
 import { BASE_URL } from '../utils/constant';
 
 const Login = () => {
-    const [emailId, setEmail] = useState('sonu@gmail.com')
-    const [password, setPassword] = useState('Sonu@123')
+    const [emailId, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [gender, setGender] = useState('')
+    const [isLoginForm, setIsLoginForm] = useState(true)
+
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -27,11 +32,54 @@ const Login = () => {
             console.log(err)
         }
     }
+
+    // Handle Sign Up
+    const handleSignUp = async () => {
+        try {
+            const res = await axios.post(BASE_URL + '/signup', {
+                firstName,
+                lastName,
+                emailId,
+                password,
+                gender,
+            },
+                { withCredentials: true }
+            )
+            dispatch(addUser(res.data.data))
+            return navigate('/profile')
+        } catch (err) {
+            console.log("error during signUP", err)
+        }
+    }
+
+
     return (
         <div className="flex justify-center mt-10">
             <div className="card bg-base-300 w-96 shadow-xl">
                 <div className="card-body">
-                    <h2 className="card-title justify-center mb-6">Login</h2>
+                    <h2 className="card-title justify-center mb-6">{isLoginForm ? "Login" : "SignUp"}</h2>
+                    {!isLoginForm && (
+                        <>
+                            <label className="form-control w-full max-w-xs">
+                                <div className="label">
+                                    <span className="label-text">First Name</span>
+                                </div>
+                                <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder="First Name" className="input input-bordered w-full max-w-xs" />
+                            </label>
+                            <label className="form-control w-full max-w-xs">
+                                <div className="label">
+                                    <span className="label-text">Last Name</span>
+                                </div>
+                                <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder="Last Name" className="input input-bordered w-full max-w-xs" />
+                            </label>
+                            <label className="form-control w-full max-w-xs">
+                                <div className="label">
+                                    <span className="label-text">Gender</span>
+                                </div>
+                                <input type="text" value={gender} onChange={(e) => setGender(e.target.value)} placeholder="Gender" className="input input-bordered w-full max-w-xs" />
+                            </label>
+                        </>)
+                    }
                     <label className="form-control w-full max-w-xs">
                         <div className="label">
                             <span className="label-text">Email ID</span>
@@ -42,11 +90,17 @@ const Login = () => {
                         <div className="label">
                             <span className="label-text">Password</span>
                         </div>
-                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="**********" className="input input-bordered w-full max-w-xs" />
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="••••••••" className="input input-bordered w-full max-w-xs" />
                     </label>
                     <div className="card-actions justify-center mt-6">
-                        <button onClick={handleLogin} className="btn btn-outline btn-primary w-full">Login</button>
+                        <button onClick={isLoginForm ? handleLogin : handleSignUp} className="btn btn-outline btn-primary w-full">{isLoginForm ? "Login" : "Sign Up"}</button>
                     </div>
+                    <p>
+                        {isLoginForm ? "Don't have an account?" : "Already have an account?"}
+                        <button className="btn btn-link" onClick={() => setIsLoginForm(!isLoginForm)}>
+                            {isLoginForm ? "Sign Up" : "Login"}
+                        </button>
+                    </p>
                 </div>
             </div>
         </div>
