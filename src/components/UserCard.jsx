c6b107f7830f2bbbf92de0c9fb6192c948cb1478
Sonu Kumar <sksonu8544@gmail.com>
2025-06-02 02:21:09 +0530

    remove crd button edit profile page

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -13,7 +13,7 @@ const UserCard = ({ user }) => {
 
     const handleSendRequest = async (status, userId) => {
         try {
-            const res = await axios.post(BASE_URL+ "/request/send/" + status + '/' + userId, {}, { withCredentials: true });
+            const res = await axios.post(BASE_URL + "/request/send/" + status + '/' + userId, {}, { withCredentials: true });
             dispatch(removeUserFromFeed(userId));
         } catch (error) {
             console.error("Error sending request:", error);
@@ -32,10 +32,12 @@ const UserCard = ({ user }) => {
                     <h2 className="card-title">{firstName + " " + lastName}</h2>
                     {age && gender && <p>{age + ' ' + gender}</p>}
                     <p>{about}</p>
-                    <div className="card-actions justify-center gap-10">
-                        <button className="btn btn-soft btn-secondary" onClick={() => handleSendRequest("ignored", _id)} >Ignore</button>
-                        <button className="btn btn-soft btn-success" onClick={() => handleSendRequest("interested", _id)}>Interested</button>
-                    </div>
+                    {_id &&
+                        <div className="card-actions justify-center gap-10">
+                            <button className="btn btn-soft btn-secondary" onClick={() => handleSendRequest("ignored", _id)} >Ignore</button>
+                            <button className="btn btn-soft btn-success" onClick={() => handleSendRequest("interested", _id)}>Interested</button>
+                        </div>
+                    }
                 </div>
             </div>
         </div>
